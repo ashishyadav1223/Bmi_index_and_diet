bfd327c5ea1aed7cb1cde923107d763054ea9786
ashishyadav1223 <ashishmyadav29@gmail.com>
2025-02-21 23:42:22 +0530

    remove unwanted files from remote reposatory

--- canvas.py
+++
@@ -1,9 +0,0 @@
-from tkinter import*
-root=Tk()
-canvas=Canvas(root,width=400,height=400,bg="black")
-canvas.pack()
-canvas.create_line(0,0,400,400,fill="white")
-canvas.create_rectangle(40,50,300,200,fill="yellow")
-canvas.create_oval(69,79,250,25,fill="red")
-canvas.create_polygon(40,50,68,79,68,89)
-root.mainloop()
--- date.py
+++
@@ -1,6 +0,0 @@
-from datetime import date
-current=date.today()
-print("current time is:",current.day)
-print("current month is:",current.month)
-print("current year is:",current.year)
-print(current.strftime("%d%m%y"))
